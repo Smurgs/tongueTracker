816eac434bb01846aa84b28cd78da227fd969b9e
Stefan <Stefan.Murga@hotmail.com>
2018-06-19 09:44:16 -0400

    only use dropout in training alexnet rgbd

--- models/RGBD_AlexNet.py
+++ models/RGBD_AlexNet.py
@@ -21,16 +21,16 @@ def build_model(rgb_x, depth_x, y, batch_size, reuse, training_ph):
     with tf.variable_scope('model'):
         with tf.variable_scope('conv_variables'):
             with tf.device('/cpu:0'):
-                conv1W = tf.get_variable('conv1W', shape=[11, 11, 4, 82], initializer=tf.contrib.layers.xavier_initializer())
-                conv1B = tf.get_variable('conv1B', shape=[82], initializer=tf.zeros_initializer())
-                conv2W = tf.get_variable('conv2W', shape=[5, 5, 82, 192], initializer=tf.contrib.layers.xavier_initializer())
-                conv2B = tf.get_variable('conv2B', shape=[192], initializer=tf.zeros_initializer())
-                conv3W = tf.get_variable('conv3W', shape=[3, 3, 192, 288], initializer=tf.contrib.layers.xavier_initializer())
-                conv3B = tf.get_variable('conv3B', shape=[288], initializer=tf.zeros_initializer())
-                conv4W = tf.get_variable('conv4W', shape=[3, 3, 288, 288], initializer=tf.contrib.layers.xavier_initializer())
-                conv4B = tf.get_variable('conv4B', shape=[288], initializer=tf.zeros_initializer())
-                conv5W = tf.get_variable('conv5W', shape=[3, 3, 288, 192], initializer=tf.contrib.layers.xavier_initializer())
-                conv5B = tf.get_variable('conv5B', shape=[192], initializer=tf.zeros_initializer())
+                conv1W = tf.get_variable('conv1W', shape=[11, 11, 4, 96], initializer=tf.contrib.layers.xavier_initializer())
+                conv1B = tf.get_variable('conv1B', shape=[96], initializer=tf.zeros_initializer())
+                conv2W = tf.get_variable('conv2W', shape=[5, 5, 96, 256], initializer=tf.contrib.layers.xavier_initializer())
+                conv2B = tf.get_variable('conv2B', shape=[256], initializer=tf.zeros_initializer())
+                conv3W = tf.get_variable('conv3W', shape=[3, 3, 256, 384], initializer=tf.contrib.layers.xavier_initializer())
+                conv3B = tf.get_variable('conv3B', shape=[384], initializer=tf.zeros_initializer())
+                conv4W = tf.get_variable('conv4W', shape=[3, 3, 384, 384], initializer=tf.contrib.layers.xavier_initializer())
+                conv4B = tf.get_variable('conv4B', shape=[384], initializer=tf.zeros_initializer())
+                conv5W = tf.get_variable('conv5W', shape=[3, 3, 384, 256], initializer=tf.contrib.layers.xavier_initializer())
+                conv5B = tf.get_variable('conv5B', shape=[256], initializer=tf.zeros_initializer())
 
         # Build graph
         with tf.variable_scope('conv1'):
@@ -50,12 +50,10 @@ def build_model(rgb_x, depth_x, y, batch_size, reuse, training_ph):
         with tf.variable_scope('max_pool3'):
             model_out = tf.nn.max_pool(model_out, [1, 3, 3, 1], [1, 2, 2, 1], 'VALID')
         model_out = tf.contrib.layers.flatten(model_out)
-        model_out = tf.nn.dropout(model_out, 0.5)
-        #model_out = tf.contrib.layers.fully_connected(model_out, 4096, reuse=reuse, scope='fc1')
-        model_out = tf.contrib.layers.fully_connected(model_out, 3072, reuse=reuse, scope='fc1')
-        model_out = tf.nn.dropout(model_out, 0.5)
-        #model_out = tf.contrib.layers.fully_connected(model_out, 4096, reuse=reuse, scope='fc2')
-        model_out = tf.contrib.layers.fully_connected(model_out, 3072, reuse=reuse, scope='fc2')
+        model_out = tf.layers.dropout(model_out, 0.5, training=training_ph)
+        model_out = tf.contrib.layers.fully_connected(model_out, 4096, reuse=reuse, scope='fc1')
+        model_out = tf.nn.dropout(model_out, 0.5, training=training_ph)
+        model_out = tf.contrib.layers.fully_connected(model_out, 4096, reuse=reuse, scope='fc2')
         model_out = tf.contrib.layers.fully_connected(model_out, 7, reuse=reuse, scope='fc3', activation_fn=None)
 
     # Inference
